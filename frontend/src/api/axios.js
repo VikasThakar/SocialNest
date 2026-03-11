65fb80235c2d6507af591812e7c259909429530b
Vikas Jamariya <vikasjamariya56@gmail.com>
2026-03-11 10:15:27 +0530

    Change API base URL to production endpoint

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://socialnest-m6i5.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
