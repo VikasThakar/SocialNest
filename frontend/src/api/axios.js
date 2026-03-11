2df51fc4300db796c733acd0cc23f35174c8ce08
Vikas Jamariya <vikasjamariya56@gmail.com>
2026-03-11 11:08:22 +0530

    Change API base URL in axios configuration

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://socialnest-m6i5.onrender.com",
+  baseURL: "https://socialnest-1-ba.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
